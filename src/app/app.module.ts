27639f05d9a18232bc33721be4bd77f9cf1d3a5c
Ali <alialmugoter@umich.edu>
2019-04-03 13:26:49 -0400

    Added team-deatial component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -8,19 +7,26 @@ import { CustomMaterialModule } from './core/material.module';
 import { NavigationComponent } from './navigation/navigation.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { ViewProfileComponent } from './view-profile/view-profile.component';
+import { TeamDetailComponent } from './team-detail/team-detail.component';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
     MainPageComponent,
-    ViewProfileComponent
+    ViewProfileComponent,
+    TeamDetailComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    CustomMaterialModule
+    CustomMaterialModule,
+    MatInputModule
+  ],
+  exports: [
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
