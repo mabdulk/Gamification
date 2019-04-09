29663adf181abc6fd55cb829519226ce83019b64
mabdulk <34901565+mabdulk@users.noreply.github.com>
2019-04-09 13:09:52 -0400

    team details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
 import { ViewProfileComponent } from './view-profile/view-profile.component';
+import { TeamDetailComponent } from './team-detail/team-detail.component';
 
 const routes: Routes = [
   { path: '', component: MainPageComponent, data: { title: 'First Component' } },
-  { path: 'profile', component: ViewProfileComponent, data: { title: 'Profile Component' } }
+  { path: 'profile', component: ViewProfileComponent, data: { title: 'Profile Component' } },
+  { path: 'team-details/:teamName', component: TeamDetailComponent, data: { title: 'Team detail Component' } }
   
 ];
 
--- src/app/team-detail/team-detail.component.ts
+++ src/app/team-detail/team-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-team-detail',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeamDetailComponent implements OnInit {
 
-  constructor() { }
+  teamName : string;
+  displayedColumns: string[] = ['teamMembers'];
+  dataSource: string[] =  ['Mustafa Abdulkareem', 'Ali-almugoter', 'Ali Tarraf ','Steve Karim'];
+;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+     this.route.params.subscribe(params => {
+      this.teamName = params['teamName']; 
+   });
   }
+  
+  
 
 }
